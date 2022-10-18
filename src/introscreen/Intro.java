c436d4991b2a72c24574648e0e6617f76f357e1b
NK <zzum0701@naver.com>
2022-10-18 12:01:33 +0900

    시작 화면

---
+++ src/introscreen/Intro.java
@@ -0,0 +1,29 @@
+package introscreen;
+
+import java.awt.Graphics;
+import java.awt.Image;
+
+import javax.swing.ImageIcon;
+import javax.swing.JFrame;
+
+public class Intro extends JFrame {
+	private Image background=new ImageIcon(Intro.class.getResource("../image/first.png")).getImage();
+	
+	public Intro() {
+		setSize(600, 900);
+		setResizable(false);
+		setLocationRelativeTo(null);
+		setVisible(true);
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+	}
+	
+	public void paint(Graphics g) {
+		g.drawImage(background, 0, 0, null);
+	}
+	
+	public static void main(String[] args) {
+		new Intro();
+
+	}
+
+}
